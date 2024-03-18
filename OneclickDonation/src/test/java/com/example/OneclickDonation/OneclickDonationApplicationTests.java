99f0c475d964cbd3fc907bf88279863a4a3c5ce5
jch4817 <jungch94@naver.com>
2024-03-18 16:04:38 +0900

    asdzxcz

--- OneclickDonation/src/test/java/com/example/OneclickDonation/OneclickDonationApplicationTests.java
+++ OneclickDonation/src/test/java/com/example/OneclickDonation/OneclickDonationApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OneclickDonationApplicationTests {
-//asdasdasdasdfasfd
+//asdasdasdasdfasfdasdfasd
 	@Test
 	void contextLoads() {
 	}

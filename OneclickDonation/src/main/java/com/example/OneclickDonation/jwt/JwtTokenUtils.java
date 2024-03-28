f9a9656495a799f47bd62fa0bbae31f64cff76d4
soeunc <fkckxk3910@gmail.com>
2024-03-28 23:24:28 +0900

    fix: 토큰에 payload에 authorities 추가

--- OneclickDonation/src/main/java/com/example/OneclickDonation/jwt/JwtTokenUtils.java
+++ OneclickDonation/src/main/java/com/example/OneclickDonation/jwt/JwtTokenUtils.java
@@ -1,16 +1,21 @@
 package com.example.OneclickDonation.jwt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.sql.Date;
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -19,12 +24,12 @@ public class JwtTokenUtils {
     private final JwtParser jwtParser;
 
     public JwtTokenUtils(
-               @Value("${jwt.secret}")
-               String jwtSecret
-       ) {
-           this.singningKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
-           this.jwtParser = Jwts.parserBuilder().setSigningKey(this.singningKey).build();
-        }
+            @Value("${jwt.secret}")
+            String jwtSecret
+    ) {
+        this.singningKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        this.jwtParser = Jwts.parserBuilder().setSigningKey(this.singningKey).build();
+    }
 
     public String generateToken(UserDetails userDetails) {
         Instant now = Instant.now();
@@ -32,6 +37,13 @@ public class JwtTokenUtils {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(Date.from(now))
                 .setExpiration(Date.from(now.plusSeconds(60 * 60 * 24 * 7 * 2)));
+
+        // 사용자의 권한 정보를 가져와서 토큰에 추가
+        String authorities = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        jwtClaims.put("authorities", authorities);
 
         log.info("토큰 정보: {}", jwtClaims);
         return Jwts.builder()
